d56add4148e2653f5b02cafa0c508e4b0bcdf70a
Anderson <andersonsilva@alu.ufc.br>
2022-12-27 21:27:03 -0300

    Modificações no populate e no back, e requests de filtros criados

--- app-viaria-frontend/src/requests/DefeitosResquest.js
+++ app-viaria-frontend/src/requests/DefeitosResquest.js
@@ -6,7 +6,20 @@ export default {
   findAll() {
     return Resquest.get(`${resource}?populate=imagem`);
   },
-  findOne() {
-    return Resquest.get(`${resource}?populate=imagem`);
+
+  findOne(id) {
+    return Resquest.get(`${resource}/${id}?populate=imagem`);
   },
+
+  findByCidadeAndRodovia(filters){
+    const cidade = filter.cidade;
+    const rodovia = filters.rodovia;
+
+    if(rodovia === null && cidade !== null )
+      return Request.get(`${resource}?filters[cidade][$eq]=${cidade}`);
+    if(cidade === null && rodovia !== null)
+      return Request.get(`${resource}?filters[endereco][$eq]=${rodovia}`);
+
+    return Request.get(`${resource}?filters[$and][0][cidade][$eq]=${cidade}&filters[$and][1][${rodovia}]`);
+  }
 };
